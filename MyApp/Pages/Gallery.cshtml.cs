183f00154d77f30f7282c5c7de12e2b197b167bb
pavlo.yutish <od210291jpv@gmail.com>
2025-06-09 16:31:24 +0300

    fixed gallery bug

--- MyApp/Pages/Gallery.cshtml.cs
+++ MyApp/Pages/Gallery.cshtml.cs
@@ -5,14 +5,6 @@ namespace MyApp.Pages
 
     public class mainModel : PageModel
     {
-        public string[] Images { get; set; }
-
-        public mainModel()
-        {
-            var imgs = Directory.GetFiles("../MyApp/wwwroot/img");
-            this.Images = imgs.Select(i => Path.GetFileName(i)).ToArray();
-        }
-
         public void OnGet()
         {
         }
