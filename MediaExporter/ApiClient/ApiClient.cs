6b52cc3b80b96386cf469b5fc2b99a970f3918c3
pavlo.yutish <od210291jpv@gmail.com>
2025-09-05 16:17:30 +0300

    added some features

---
+++ MediaExporter/ApiClient/ApiClient.cs
@@ -0,0 +1,32 @@
+﻿using RestSharp;
+using RestSharp.Serializers.NewtonsoftJson;
+using System.Net;
+
+namespace MediaExporter.ApiClient
+{
+    public class ApiClient
+    {
+        public async Task<HttpStatusCode[]> ExportMedia(string[] path) 
+        {
+            RestSharp.RestClient client = new RestSharp.RestClient(configureSerialization: s => s.UseNewtonsoftJson());
+            List< HttpStatusCode > responses = new List<HttpStatusCode>();
+
+            int i = 0;
+
+            foreach (var med in path) 
+            {
+                i++;
+                Console.WriteLine($"Exporting: {i}/{path.Length} {med} to server...");
+                await Task.Delay(100);
+                RestSharp.RestRequest request = new RestSharp.RestRequest(UriHelper.SendMedia(med.Split("/").Last(), hidden: false), RestSharp.Method.Post);
+                request.AddFile("File", med, "image/jpeg");
+
+                RestResponse response = await client.ExecuteAsync(request);
+                Console.WriteLine($"{med} export, status is :{response.StatusCode}");
+                responses.Add(response.StatusCode);
+            }
+
+            return responses.ToArray();
+        }
+    }
+}
---
+++ MediaExporter/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MediaExporter.ApiClient;
+
+Console.WriteLine("Hello, World!");
+
+string[] files = Directory.GetFiles(@"Z:\Paul\ing");
+foreach (var f in files) 
+{
+    File.Move(f, @$"Z:\Paul\ing\{Guid.NewGuid().ToString().Replace("-", "")}.{f.Split(".").Last()}");
+}
+
+files = Directory.GetFiles(@"Z:\Paul\ing");
+
+ApiClient apiClient = new ApiClient();
+
+await apiClient.ExportMedia(files);
