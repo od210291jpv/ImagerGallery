7f667ce96df3d5a48363f82276a726769f8e4da1
pavlo.yutish <od210291jpv@gmail.com>
2026-02-13 11:17:12 +0200

    commented redundant code in media exporter

--- MediaExporter/Program.cs
+++ MediaExporter/Program.cs
@@ -4,32 +4,32 @@ using MediaExporter.ApiClient;
 using System.Text.RegularExpressions;
 
 //string[] files = Directory.GetFiles(@"Z:\Paul\ing");
-string[] files = Directory.GetFiles(@"Z:\Paul\ing");
-string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+//string[] files = Directory.GetFiles(@"Z:\Paul\ing");
+//string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 
-foreach (var f in files) 
-{
-    string fileExtention = f.Split(".").Last();
-    string namePrefix = "";
-    var debug = f.Split(@"\").Last().Replace(fileExtention, "");
-    Faker faker = new Faker("en");
-    if (!chars.Any(c => f.Split(@"\").Last().Replace(fileExtention, "").Contains(c)))
-    {
-        namePrefix = $"{faker.Name.FirstName()}.";
-    }
-    else 
-    {
-        string randomStr = chars[new Random().Next(0, chars.Length - 1)].ToString();
-        namePrefix = Regex.Replace(f.Split(@"\").Last().Replace(fileExtention, ""), @"\d", randomStr);
-    }
+//foreach (var f in files) 
+//{
+//    string fileExtention = f.Split(".").Last();
+//    string namePrefix = "";
+//    var debug = f.Split(@"\").Last().Replace(fileExtention, "");
+//    Faker faker = new Faker("en");
+//    if (!chars.Any(c => f.Split(@"\").Last().Replace(fileExtention, "").Contains(c)))
+//    {
+//        namePrefix = $"{faker.Name.FirstName()}.";
+//    }
+//    else 
+//    {
+//        string randomStr = chars[new Random().Next(0, chars.Length - 1)].ToString();
+//        namePrefix = Regex.Replace(f.Split(@"\").Last().Replace(fileExtention, ""), @"\d", randomStr);
+//    }
 
-    string newFileName = @$"Z:\Paul\ing\{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}_{namePrefix}{fileExtention}";
+//    string newFileName = @$"Z:\Paul\ing\{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}_{namePrefix}{fileExtention}";
 
-    File.Move(f, newFileName);
-}
+//    File.Move(f, newFileName);
+//}
 
-files = Directory.GetFiles(@"Z:\Paul\ing");
+var files = Directory.GetFiles(@"Z:\Paul\ing");
 
 ApiClient apiClient = new ApiClient();
 
